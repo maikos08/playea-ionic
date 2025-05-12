92354904720861c8a204f03f03c593e05938764d
Miguel Ángel <migue.rodrii08@gmail.com>
2025-05-12 15:53:05 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'playea-ionic',
+  webDir: 'www'
+};
+
+export default config;
