bd9dfffbb1e2a2129b667d6636df68d0384b3d78
Miguel Ángel <migue.rodrii08@gmail.com>
2025-05-20 19:40:18 +0100

    feat: update Angular dependencies and add Firebase support
    - Updated @angular/fire to version 19.2.0 in package.json.
    - Changed logo image source in header component.
    - Created login component with basic template and styles.
    - Created register component with basic template and styles.
    - Updated home component styles to correct image path.
    - Added new images for logo and background.
    - Modified main.ts to include Firebase configuration.

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ bootstrapApplication(AppComponent, {
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
-    provideIonicAngular(),
+    provideIonicAngular(), provideFirebaseApp(() => initializeApp({ projectId: "playea-eu", appId: "1:706207252910:web:9f3c1c1b2d107c400a883f", storageBucket: "playea-eu.firebasestorage.app", apiKey: "AIzaSyDekO0aTS3WZL2ocg9Kb1ewNTZ7FjRpBGs", authDomain: "playea-eu.firebaseapp.com", messagingSenderId: "706207252910", measurementId: "G-MYWVFD58B8" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
   ],
 });
